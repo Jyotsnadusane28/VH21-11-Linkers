88affbd1cf3180e7b76883f9b3f4de0bd472d77d
jyotsna dusane <51448568+Jyotsnadusane28@users.noreply.github.com>
2021-03-19 22:24:20 +0530

    Add files via upload
    Added ARBook Feature

---
+++ CarLeft.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class CarLeft : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+	bool ispressed = false;
+	public GameObject player;
+
+
+	// Update is called once per frame
+	void Update () {
+		if (ispressed) {
+			player.transform.Translate (-0.2f,0,0);
+		}
+	}
+
+	public void OnPointerDown(PointerEventData evenData){
+		ispressed = true;
+	}
+	public void OnPointerUp(PointerEventData evenData){
+		ispressed = false;
+	}
+
+}
---
+++ CarRight.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class CarRight : MonoBehaviour {
+	bool ispressed = false;
+	public GameObject player;
+
+	void Update () {
+		if (ispressed) {
+			player.transform.Translate (0.2f,0,0);
+		}
+	}
+	public void OnPointerDown(PointerEventData evenData){
+		ispressed = true;
+	}
+	public void OnPointerUp(PointerEventData evenData){
+		ispressed = false;
+	}
+
+}
